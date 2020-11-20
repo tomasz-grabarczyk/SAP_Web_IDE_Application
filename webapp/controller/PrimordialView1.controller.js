a7eb7959a3c3403c62bedef4d59319809469182e
tomasz-grabarczyk <tomas.grabarczyk@gmail.com>
2020-11-20 23:01:04 +0000

    Added JSON model

--- webapp/controller/PrimordialView1.controller.js
+++ webapp/controller/PrimordialView1.controller.js
@@ -1,16 +1,22 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
-		
+
 	return Controller.extend("PrimordialApp.controller.PrimordialView1", {
-		onShowHello : function () {
+		onInit: function () {
+			var oData = {
+				recipient: {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+		onShowHello: function () {
 			MessageToast.show("Oh... Hello there!");
 		}
-	}); 
+	});
 });
-	
-	
-	
-
